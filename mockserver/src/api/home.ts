ca93888751a5bec7095ec102abb933552c890cde
tsusowake <tsusowake@gmail.com>
2023-07-16 22:48:18 +0900

    fix

--- mockserver/src/api/home.ts
+++ mockserver/src/api/home.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { APISetUpper } from '.'
 import { randomWait } from '../util/random_wait'
-import {  homecontent } from './gen/protobuf_generated'
+import { homecontent } from './gen/protobuf_generated'
 import { sendResponse } from '../util/send_response'
 
 export const setupHomeAPI = (r: Router): void => {
@@ -11,83 +11,80 @@ export const setupHomeAPI = (r: Router): void => {
   r.get('/home/contents', async (req, res) => {
     await randomWait(2)
 
-    sendResponse(
-      res,
-      homecontent.HomeContentResponse.encode(homeContents).finish(),
-    )
+    sendResponse(res, homecontent.HomeContentResponse.encode(homeContents).finish())
   })
 }
 
 const contents: homecontent.IContent[] = [
   {
-    title: "ぶんちょうぶんちょうぶんちょうぶんちょうぶんちょう",
-    thumbnail: "/img/buncho.1.jpg",
+    title: 'ぶんちょうぶんちょうぶんちょうぶんちょうぶんちょう',
+    thumbnail: '/img/buncho.1.jpg',
   },
   {
-    title: "ブンチョウブンチョウブンチョウブンチョウブンチョウ",
-    thumbnail: "/img/buncho.2.jpg",
+    title: 'ブンチョウブンチョウブンチョウブンチョウブンチョウ',
+    thumbnail: '/img/buncho.2.jpg',
   },
   {
-    title: "文鳥文鳥文鳥文鳥文鳥文鳥文鳥文鳥文鳥文鳥",
-    thumbnail: "/img/buncho.3.jpg",
+    title: '文鳥文鳥文鳥文鳥文鳥文鳥文鳥文鳥文鳥文鳥',
+    thumbnail: '/img/buncho.3.jpg',
   },
   {
-    title: "bunchobunchobunchobunchobunchobuncho",
-    thumbnail: "/img/buncho.4.jpg",
+    title: 'bunchobunchobunchobunchobunchobuncho',
+    thumbnail: '/img/buncho.4.jpg',
   },
   {
-    title: "部長部長部長部長部長部長部長部長部長部長部長",
-    thumbnail: "/img/buncho.5.jpg",
+    title: '部長部長部長部長部長部長部長部長部長部長部長',
+    thumbnail: '/img/buncho.5.jpg',
   },
   {
-    title: "ぶんちょう",
-    thumbnail: "/img/buncho.6.jpg",
+    title: 'ぶんちょう',
+    thumbnail: '/img/buncho.6.jpg',
   },
   {
-    title: "ブンチョウ",
-    thumbnail: "/img/buncho.7.jpg",
+    title: 'ブンチョウ',
+    thumbnail: '/img/buncho.7.jpg',
   },
   {
-    title: "文鳥",
-    thumbnail: "/img/buncho.8.jpg",
+    title: '文鳥',
+    thumbnail: '/img/buncho.8.jpg',
   },
   {
-    title: "buncho",
-    thumbnail: "/img/buncho.9.jpg",
+    title: 'buncho',
+    thumbnail: '/img/buncho.9.jpg',
   },
   {
-    title: "部長",
-    thumbnail: "/img/buncho.10.jpg",
+    title: '部長',
+    thumbnail: '/img/buncho.10.jpg',
   },
 ]
 const homeContents: homecontent.HomeContentResponse = {
   contentRows: [
     {
-      rowHeader: "新作",
+      rowHeader: '新作',
       contentType: homecontent.ContentType.CONTENT_TYPE_VIDEO,
-      contents: contents
+      contents: contents,
     },
     {
-      rowHeader: "検索で人気の作品",
+      rowHeader: '検索で人気の作品',
       contentType: homecontent.ContentType.CONTENT_TYPE_VIDEO,
-      contents: contents
+      contents: contents,
     },
     {
-      rowHeader: "視聴中コンテンツ",
+      rowHeader: '視聴中コンテンツ',
       contentType: homecontent.ContentType.CONTENT_TYPE_VIDEO,
-      contents: contents
+      contents: contents,
     },
     {
-      rowHeader: "人気急上昇の作品",
+      rowHeader: '人気急上昇の作品',
       contentType: homecontent.ContentType.CONTENT_TYPE_VIDEO,
-      contents: contents
+      contents: contents,
     },
     {
-      rowHeader: "「文鳥」をご覧になったあなたへ",
+      rowHeader: '「文鳥」をご覧になったあなたへ',
       contentType: homecontent.ContentType.CONTENT_TYPE_VIDEO,
-      contents: contents
+      contents: contents,
     },
-  ]
+  ],
 }
 
 export default setupHomeAPI as APISetUpper
